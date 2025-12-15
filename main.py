5af71d8fd668651a55d7f8784af407d2786064f0
fangjun <fjun99@gmail.com>
2025-12-15 10:25:23 +0800

    feat: revise main to accept cli args

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import sys
 from cowsay_demo import cowsay
 
 def main():
     print("Hello from gitsample-project!")
-    cowsay("Hello from main!")
+    message = sys.argv[1] if len(sys.argv) > 1 else "Hello from main!"
+    cowsay(message)
 
 
 if __name__ == "__main__":
